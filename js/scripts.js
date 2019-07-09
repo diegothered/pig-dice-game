f7fa03702cb0c96ae70f5f49900e88ccd233450c
Diego Rojas and Jaime Takikawa <diego@>
2019-07-08 20:38:29 -0700

    add two lines to prototype branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
+//player roll random roll generator
 function myRoll() {
   var x = Math.floor((Math.random() * 6) + 1);
   document.getElementById("showRoll").innerHTML = x;
+}
+
+//Business Logic
+// This is a constructor for Player to store all player info
+function Player(playerOne, turnTotal, diceRoll, overallScore) {
+  this.playerOne = playerOne,
+  this.turnTotal = turnTotal,
+  this.diceRoll = diceRoll,
+  this.overallScore = overallScore
+}
+
+//prototype branching for possible player roll combinations
+Player.prototype.roll = function () {
+  if (x >= 2 && x <= 6) {
+
+  }
+  else {
+
+  }
+  return this.diceRoll;
 }
